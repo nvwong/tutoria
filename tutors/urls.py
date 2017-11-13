a8faa106b524d1e51b11ab9f633e944686a194bb
nvwong <27146493+nvwong@users.noreply.github.com>
2017-11-13 22:38:13 +0800

    updated models, implemented search

--- tutors/urls.py
+++ tutors/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'tutors'
 urlpatterns = [
     url(r'^$', views.indexView.as_view(), name='index'),
+    url(r'^search/$', views.search, name='tutor_search'),
+    url(r'^search/results', views.searchResultView.as_view(), name='tutor_search_result'),
     url(r'^(?P<tutor_id>[0-9]+)/$', views.tutors, name='tutors')
 ]

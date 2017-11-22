efee7a6988a5ff5f190bfd4c0c3a71183a736d04
cybcheng <33854824+cybcheng@users.noreply.github.com>
2017-11-22 15:18:17 +0800

    Add files via upload

--- tutors/views.py
+++ tutors/views.py
@@ -89,3 +89,10 @@ class MyWallet(generic.ListView):
         trans_history = session.filter(isLocked=True)
         return trans_history
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(MyWallet, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['user'] = self.request.user
+        return context
+

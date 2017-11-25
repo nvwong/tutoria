6d6afebdac1680b9746caea96af821b4e583c1d2
nvwong <27146493+nvwong@users.noreply.github.com>
2017-11-25 09:51:05 +0800

    Make Booking

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.ShowOneSession.as_view(), name='oneSession'),
     url(r'^cancel/$', views.sessionListView.as_view(), name='session_list'),
     url(r'^cancel/make_a_cancel/$', views.cancel, name='make_cancel'),
-    #url(r'^cancel/ok/$', views.CancelokView.as_view(), name='cancel_ok'),
+    url(r'^book/(?P<tutor_id>[0-9]+)/', views.MakeBooking.as_view(), name='make_booking'),
     # url(r'^$', views.indexView.as_view(), name='index'),
 ]
--- tutorial/views.py
+++ tutorial/views.py
@@ -5,8 +5,13 @@ from django.core import mail
 from django.urls import reverse
 #from django.core.mail import EmailMessage
 from .models import Session
-from tutors.models import Tutor
+from tutors.models import Tutor, NotAvailableSlot
 from students.models import Student
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.utils import timezone
+import datetime
 
 # Create your views here.
 class ShowOneSession(generic.DetailView):
@@ -30,6 +35,77 @@ class sessionListView(generic.ListView):
         context['user'] = self.request.user # you can add template variables!
         return context
 
+class MakeBooking (SuccessMessageMixin, generic.CreateView):
+    model = Session
+    fields = ['start_time']
+    template_name = "bookSession.html"
+    success_url = reverse_lazy('students:mybookings')
+    success_message = 'Booking Successful.'
+
+    def form_valid(self, form):
+        ok = True
+
+        the_student = Student.objects.get(student=self.request.user)
+        the_tutor = Tutor.objects.get(pk=self.kwargs['tutor_id'])
+        notAvailable = NotAvailableSlot.objects.filter(tutor=the_tutor)
+        st = form.instance.start_time
+        et = st + datetime.timedelta(minutes=the_tutor.timePerSlot)
+
+        for slot in notAvailable:
+            if (slot.start_time <= st) and (slot.end_time >= et):
+                ok = False
+                messages.error(self.request, 'Crashed with Tutor Unavailable Slots.')
+
+        if ok:
+            mylist = Session.objects.filter(student=the_student)
+            for booking in mylist:
+                if (booking.start_time.date() == st.date()) and (booking.tutor == the_tutor):
+                    ok = False
+                    messages.error(self.request, 'Cannot book more than one slot of same tutor in a day.')
+
+            if (st - timezone.now() <= datetime.timedelta(hours=24)):
+                ok = False
+                messages.error(self.request, 'Cannot book session within coming 24 hours.')
+
+            if (st - timezone.now() > datetime.timedelta(days=7)):
+                ok = False
+                messages.error(self.request, 'Cannot book further than 7 days.')
+
+            if (the_student.wallet < (the_tutor.hourlyRate)*1.05):
+                ok = False
+                messages.error(self.request, 'You do not have enough money.')
+
+        if ok:
+            form.instance.student = the_student
+            form.instance.tutor = the_tutor
+            form.instance.end_time = et
+            new_unavail = NotAvailableSlot(tutor=the_tutor, start_time=st, end_time=et)
+            new_unavail.save()
+            the_student.wallet -= (the_tutor.hourlyRate)*1.05
+            the_student.save()
+
+            body ='Dear ' + the_tutor.tutor.get_full_name() + ',\n' + 'A session is booked by ' + the_student.student.get_full_name() + ' from ' + str(st) + ' to ' + str(et) + '. Go to Tutoria Homapage to check it out.\nTutoria'
+            mail.send_mail('A session is booked', body, 'admin@tutoria.com', [the_tutor.tutor.email])
+
+            body ='Dear ' + the_student.student.get_full_name() + ',\n' + 'A session taught by '+ the_tutor.tutor.get_full_name() +' from ' + str(st) + ' to ' + str(et) + ' is booked. Your wallet value now is: $'+ str(the_student.wallet) +'. Go to Tutoria Homepage to check it out.\nTutoria'
+            mail.send_mail('A session is cancelled', body, 'admin@tutoria.com', [the_student.student.email])
+
+            return super(MakeBooking, self).form_valid(form)
+        else:
+            return super(MakeBooking, self).form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(MakeBooking, self).get_context_data(**kwargs)
+        #If you dont call 'super', you wont have the context processor varibles
+        #  like 'user'
+        tut = Tutor.objects.get(pk=self.kwargs['tutor_id'])
+        context['tutor'] = tut
+        context['fee'] = tut.hourlyRate*1.05
+        context['unavailability_list'] = NotAvailableSlot.objects.all()
+        return context
+
+
+
 class CancelokView(generic.DetailView):
     model = Session
     template_name = 'cancelok.html'
@@ -43,16 +119,18 @@ def cancel(request):
     else:
         student = request.user
         amount = selected_session.tutor.hourlyRate * 1.05
-        student.student.wallet +=  amount
+        student.student.wallet += amount
         #myTutors.wallet -= amount #transfer money from myTutor to student
         student.save()
         endtime = str(selected_session.end_time)
-        body ='Dear ' + selected_session.tutor.tutor.get_full_name() + ',\n' + 'A session booked by ' + student.student.username + ' from ' + str(selected_session.start_time) + ' to ' + str(selected_session.end_time) + ' is cancelled. Go to Tutoria Homapage to check it out.\nTutoria'
+        body ='Dear ' + selected_session.tutor.tutor.get_full_name() + ',\n' + 'A session booked by ' + student.username + ' from ' + str(selected_session.start_time) + ' to ' + str(selected_session.end_time) + ' is cancelled. Go to Tutoria Homapage to check it out.\nTutoria'
         mail.send_mail('A session is cancelled', body, 'admin@tutoria.com', [selected_session.tutor.tutor.email])
 
-        body ='Dear ' + selected_session.student.student.get_full_name() + ',\n' + 'A session taught by '+ selected_session.tutor.tutor.username +' from ' + str(selected_session.start_time) + ' to ' + str(selected_session.end_time) + ' is cancelled. Your wallet value now is: $'+ str(student.student.wallet) +'. Go to Tutoria Homapage to check it out.\nTutoria'
+        body ='Dear ' + selected_session.student.student.get_full_name() + ',\n' + 'A session taught by '+ selected_session.tutor.tutor.username +' from ' + str(selected_session.start_time) + ' to ' + str(selected_session.end_time) + ' is cancelled. Your wallet value now is: $'+ str(student.student.wallet) +'. Go to Tutoria Homepage to check it out.\nTutoria'
         mail.send_mail('A session is cancelled', body, 'admin@tutoria.com', [selected_session.student.student.email])
 
+        unavail = NotAvailableSlot.objects.get(tutor=selected_session.tutor, start_time=selected_session.start_time, end_time=selected_session.end_time)
+        unavail.delete()
         selected_session.delete()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a

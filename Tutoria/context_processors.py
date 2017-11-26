a015d124840a5c6b726f1facf4267be7fc6ff56c
nvwong <27146493+nvwong@users.noreply.github.com>
2017-11-26 22:06:44 +0800

    fixed admin template

--- Tutoria/context_processors.py
+++ Tutoria/context_processors.py
@@ -5,6 +5,8 @@ def base_template_name_context_processor(request):
     base_template_name = 'base.html'
 
     if request.user.is_authenticated():
+        if request.user.is_superuser:
+            base_template_name = 'base_tutor.html'
         if Tutor.objects.filter(tutor=request.user):
             base_template_name = 'base_tutor.html'
 
